42f71a93e2db4b5813249b889b17a2f228f5eb79
Steve Benton <steve@bentonemail.uk>
2026-06-10 14:16:47 +0100

    setup project

---
+++ app.js
@@ -0,0 +1,6 @@
+// Example 1 -  Welcome to Part 2!
+console.log("\nSECTION 1 - Welcome to Part 2!");
+
+// Example 2 -  Join Our Community
+console.log("\nSECTION 2 - Join Our Community");
+// No Code
