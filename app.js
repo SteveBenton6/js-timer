3f9de2cf6ead24bf553906834c663bef15a9c440
Steve Benton <steve@bentonemail.uk>
2026-06-11 12:36:03 +0100

    create seperate timer.js file for the Timer Class

--- app.js
+++ app.js
@@ -8,57 +8,6 @@ console.log("\nSECTION 2 - Join Our Community");
 // Example 3 -  App Overview
 console.log("\nSECTION 2 - App Overview");
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    // console.log("Time to start the timer!");
-    this.interval = setInterval(this.tick, 1000);
-    // console.log(this.interval);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-      if (this.onComplete) {
-        this.onComplete();
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      if (this.onTick) {
-        this.onTick();
-      }
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
