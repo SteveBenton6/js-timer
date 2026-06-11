2619f677eabd1e192d7bb49cef8528c46ea75a23
Steve Benton <steve@bentonemail.uk>
2026-06-11 12:26:04 +0100

    add callback functions for onStart, onTick and onComplete

--- app.js
+++ app.js
@@ -5,40 +5,72 @@ console.log("\nSECTION 1 - Welcome to Part 2!");
 console.log("\nSECTION 2 - Join Our Community");
 // No Code
 
+// Example 3 -  App Overview
+console.log("\nSECTION 2 - App Overview");
+
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
     this.startButton.addEventListener("click", this.start);
     this.pauseButton.addEventListener("click", this.pause);
   }
+
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
-    console.log("Time to start the timer!");
+    // console.log("Time to start the timer!");
     this.interval = setInterval(this.tick, 1000);
-    console.log(this.interval);
+    // console.log(this.interval);
   };
+
   pause = () => {
     clearInterval(this.interval);
   };
+
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
+
   get timeRemaining() {
     return parseFloat(this.durationInput.value);
   }
+
   set timeRemaining(time) {
     this.durationInput.value = time;
   }
 }
+
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer ticked down");
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
